3eaddac3a3597621a71a1a8713a2a49257289d92
Rindy Fisk <117949294+photocabana@users.noreply.github.com>
2023-04-27 01:03:47 -0500

    Add files via upload

---
+++ Rooster_or_Hen/script.js
@@ -0,0 +1,12 @@
+var img =  ["img/20230422_105607.jpg", "img/arms.jpg", "img/girl.png"]
+
+function over() {
+    chicken_switcher.src="img/arms.jpg"
+}
+
+// function move() {
+//     chicken_switcher.src="img/girl.png"
+// }
+function out() {
+    chicken_switcher.src="img/20230422_105607.jpg"
+}
